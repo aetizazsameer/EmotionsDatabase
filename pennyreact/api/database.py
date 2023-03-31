1679a9363fad581980c0c9a9d48b70c577e5d2bc
Tyler Vu <tv3433@princeton.edu>
2023-03-31 01:42:23 -0400

    query str

--- pennyreact/api/database.py
+++ pennyreact/api/database.py
@@ -1,34 +1,35 @@
 #!/usr/bin/env python
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 # database.py
 # query: Aetizaz Sameer
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 
 import video as videomod
 import psycopg2
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
 
 _HOST_URL = 'dpg-cgifmlpr8t1g7lp9krfg-a.ohio-postgres.render.com'
 _DATABASE = 'emotionsdatabase'
 _USERNAME = 'emotionsdatabase_user'
 _PASSWORD = 'muO6WwujmxvrVuxwhYRcK1jOrQslGrTm'
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
+
 
 def get_videos(query):
 
     videos = []
 
     with psycopg2.connect(host=_HOST_URL, database=_DATABASE,
-        user=_USERNAME, password=_PASSWORD) as conn:
+                          user=_USERNAME, password=_PASSWORD) as conn:
 
         with conn.cursor() as cursor:
-
-            query_str = "SELECT id, title, url FROM videos "
-            query_str += "WHERE title LIKE ? OR url LIKE ?"
-            cursor.execute(query_str, [query+'%']*2)
+            query_str = "SELECT id, title, url FROM videos " +\
+                        "WHERE videos.title LIKE ? OR videos.url LIKE ?"
+            query = f'%{query}%'
+            cursor.execute(query_str, (query, query))
 
             table = cursor.fetchall()
             for row in table:
@@ -37,7 +38,8 @@ def get_videos(query):
 
     return videos
 
-#-----------------------------------------------------------------------
+# -----------------------------------------------------------------------
+
 
 def _testhelp(query):
     videos = get_videos(query)
@@ -48,9 +50,11 @@ def _testhelp(query):
         print(video.get_uploadtimestamp())
         print()
 
+
 def _test():
     _testhelp('highlight')
-    _testhelp('neutral')
+    # _testhelp('neutral')
+
 
 if __name__ == '__main__':
     _test()
