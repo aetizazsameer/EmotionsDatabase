c8bf23938f581b8a953aae9940e37b76ad3fb37e
akhwang47 <andrewhwang@princeton.edu>
2023-03-31 00:55:11 -0400

    push push

--- pennyreact/api/script.py
+++ pennyreact/api/script.py
@@ -2,8 +2,12 @@ DB_NAME = "emotionsdatabase"
 DB_USER = "emotionsdatabase_user"
 DB_PASS = "muO6WwujmxvrVuxwhYRcK1jOrQslGrTm"
 DB_HOST = "dpg-cgifmlpr8t1g7lp9krfg-a.ohio-postgres.render.com"
+DB_PORT = "5432"
 
 import psycopg2
+
+try:
+    connection = psycopg2.connect(user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT)
 
 connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
 
