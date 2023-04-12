5f66f895fd853171bbd891dc393cb31dc1bc090e
Tyler Vu <tv3433@princeton.edu>
2023-04-12 01:09:13 -0400

    added css file + clickable grid ready for deploy

--- pennyreact/src/Grid.jsx
+++ pennyreact/src/Grid.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './Grid.css';
 
 function Grid() {
   const [gridData, setGridData] = useState(Array(50).fill(Array(50).fill(false)));
@@ -8,52 +9,22 @@ function Grid() {
   };
 
   return (
-    <div style={{ position: 'relative' }}>
-      {gridData.map((rowData, rowIndex) => (
-        <div key={rowIndex} style={{ display: 'flex' }}>
-          {rowData.map((colData, colIndex) => (
-            <span
-              key={colIndex}
-              onClick={() => handleClick(rowIndex, colIndex)}
-              style={{
-                width: '10px',
-                height: '10px',
-                border: 'none',
-                boxSizing: 'border-box',
-                flexShrink: 0,
-                borderTop: rowIndex === 0 ? '1px solid black' : 'none',
-                borderRight: colIndex === 49 ? '1px solid black' : 'none',
-                borderBottom: rowIndex === 49 ? '1px solid black' : 'none',
-                borderLeft: colIndex === 0 ? '1px solid black' : 'none',
-              }}
-            />
-          ))}
-        </div>
-      ))}
-      <div
-        style={{
-          position: 'absolute',
-          top: '50%',
-          left: 0,
-          width: '100%',
-          height: '1px',
-          backgroundColor: 'black',
-          borderTop: 'none',
-          borderBottom: 'none',
-        }}
-      />
-      <div
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: '50%',
-          width: '1px',
-          height: '100%',
-          backgroundColor: 'black',
-          borderLeft: 'none',
-          borderRight: 'none',
-        }}
-      />
+    <div className="grid">
+      <div className="grid-container">
+        {gridData.map((rowData, rowIndex) => (
+          <div key={rowIndex} className="grid-row">
+            {rowData.map((colData, colIndex) => (
+              <span
+                key={colIndex}
+                onClick={() => handleClick(rowIndex, colIndex)}
+                className={`grid-cell ${rowIndex === 0 ? 'grid-cell--top' : ''} ${colIndex === 49 ? 'grid-cell--right' : ''} ${rowIndex === 49 ? 'grid-cell--bottom' : ''} ${colIndex === 0 ? 'grid-cell--left' : ''}`}
+              />
+            ))}
+          </div>
+        ))}
+        <div className="grid-line--horizontal" />
+        <div className="grid-line--vertical" />
+      </div>
     </div>
   );
 }
