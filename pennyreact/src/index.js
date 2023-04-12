d157d04859d0ee974a99c5e2e96b90250cbc5e5d
(´・◡・｀) <67237406+aetizazsameer@users.noreply.github.com>
2023-04-11 23:59:57 -0400

    Router attempt

--- pennyreact/src/index.js
+++ pennyreact/src/index.js
@@ -5,35 +5,32 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 import Landing from './landing';
 import Admin from './admin';
 import Researcher from './researcher';
 import Participant from './participant';
 
 ReactDOM.render(
-
-    <div>
-        <strong>text</strong>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/">
-                    <Landing />
-                </Route>
-                <Route path="/index">,
-                    <Landing />
-                </Route>
-                <Route path="/admin">
-                    <Admin />
-                </Route>
-                <Route path="/researcher">
-                    <Researcher />
-                </Route>
-                <Route path="/participant">
-                    <Participant />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    </div>,
+    <BrowserRouter>
+        <Routes>
+            <Route path="/">
+                <Landing />
+            </Route>
+            <Route path="/index">,
+                <Landing />
+            </Route>
+            <Route path="/admin">
+                <Admin />
+            </Route>
+            <Route path="/researcher">
+                <Researcher />
+            </Route>
+            <Route path="/participant">
+                <Participant />
+            </Route>
+        </Routes>
+        <Outlet />
+    </BrowserRouter>,
     document.getElementById('root')
 );
