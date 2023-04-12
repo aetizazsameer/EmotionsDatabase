e4b58810af1af9ae0a57e5e7ef561ad662a6b536
(´・◡・｀) <67237406+aetizazsameer@users.noreply.github.com>
2023-04-11 21:51:19 -0400

    Interface outline

---
+++ pennyreact/src/admin.js
@@ -0,0 +1,25 @@
+//----------------------------------------------------------------------
+// admin.js
+// Author: Aetizaz Sameer, Andrew Hwang, Tyler Vu
+//----------------------------------------------------------------------
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PennyHeader from './PennyHeader.jsx';
+import PennySearch from './PennySearch.jsx';
+import PennyFooter from './PennyFooter.jsx';
+import Table from './Table.jsx';
+import Form from './Form.jsx';
+import Grid from './Grid.jsx';
+
+ReactDOM.render(
+    <div>
+        <PennyHeader />
+        <Grid />,
+        <PennySearch />
+        <Form />
+        <Table />
+        <PennyFooter />
+    </div>,
+    document.getElementById('root_admin')
+);
--- pennyreact/src/participant.js
+++ pennyreact/src/participant.js
@@ -1,22 +1,18 @@
 //----------------------------------------------------------------------
-// index.js
+// participant.js
 // Author: Aetizaz Sameer, Andrew Hwang, Tyler Vu
 //----------------------------------------------------------------------
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PennyHeader from './PennyHeader';
 import PennyFooter from './PennyFooter';
 
 ReactDOM.render(
     <div>
         <PennyHeader />
-        <a href="/admin">Admin login</a>
-        <br/>
-        <a href="/researcher">Researcher login</a>
-        <br/>
-        <a href="/participant">Participant login</a>
-        <br />
+        participant
         <PennyFooter />
     </div>,
-    document.getElementById('root')
+    document.getElementById('root_participant')
 );
