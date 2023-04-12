d3489db6b92db6d8154585f7bbb178d611614a5e
Tyler Vu <tv3433@princeton.edu>
2023-04-12 19:07:55 -0400

    axios

--- pennyreact/src/Video.jsx
+++ pennyreact/src/Video.jsx
@@ -1,13 +1,29 @@
-const src =
-  "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4";
+//----------------------------------------------------------------------
+// Video.jsx
+// Author: Aetizaz Sameer, Andrew Hwang, Tyler Vu
+//----------------------------------------------------------------------
+
+import axios from 'axios';
 
 const Video = () => {
-  return (
-    <video controls width="100%">
-      <source src={src} type="video/mp4" />
-      Sorry, your browser doesn't support embedded videos.
-    </video>
-  );
+    const [src, setSrc] = useState('');
+
+    useEffect(() => {
+      axios.get('/participant/get_URL')
+        .then(response => {
+          setSrc(response.data.url);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }, []);
+
+    return (
+        <video controls width="100%">
+        <source src={src} type="video/mp4" />
+            Sorry, your browser doesn't support embedded videos.
+        </video>
+    );
 };
 
 export default Video;
