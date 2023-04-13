c1d63956eb96992f8db2d2d35f3e190e347d193f
aetizazsameer <asameer@princeton.edu>
2023-04-13 01:41:59 -0400

    Fix useEffect import

--- pennyreact/src/Table.jsx
+++ pennyreact/src/Table.jsx
@@ -3,7 +3,7 @@
 // Author: Tyler Vu, Aetizaz Sameer
 //----------------------------------------------------------------------
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const SortableTable = () => {
