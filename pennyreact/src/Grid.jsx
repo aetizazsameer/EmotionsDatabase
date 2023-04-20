fb4f2c7d2911b4e1761002ec6d3d81e8c41f7212
Tyler Vu <tv3433@princeton.edu>
2023-04-20 05:49:03 -0400

    button appears after click + formatting

--- pennyreact/src/Grid.jsx
+++ pennyreact/src/Grid.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
-import axios from 'axios';
 import './Grid.css';
 
 
@@ -11,11 +10,13 @@ function Grid() {
   const [selectedRow, setRow] = useState(null);
   const [selectedCol, setCol] = useState(null);
   const [gridData, setGridData] = useState(Array(50).fill(Array(50).fill(false)));
+  const [showSubmitButton, setShowSubmitButton] = useState(false);
 
   const handleClick = (row, col) => {
     console.log(`Clicked on row ${row} and column ${col}`);
     setRow(row);
     setCol(col);
+    setShowSubmitButton(true);
   };
 
   const handleSubmitButton = (row, col) => {
@@ -25,28 +26,35 @@ function Grid() {
   };
 
   return (
-    <div className="grid">
-      <div className="grid-container">
-        {gridData.map((rowData, rowIndex) => (
-          <div key={rowIndex} className="grid-row">
-            {rowData.map((colData, colIndex) => (
-              <span
-                key={colIndex}
-                onClick={() => handleClick(rowIndex, colIndex)}
-                className={`grid-cell ${rowIndex === 0 ? 'grid-cell--top' : ''} ${colIndex === 49 ? 'grid-cell--right' : ''} ${rowIndex === 49 ? 'grid-cell--bottom' : ''} ${colIndex === 0 ? 'grid-cell--left' : ''} ${selectedRow === rowIndex && selectedCol === colIndex ? 'grid-cell--selected' : ''}`}
-              />
-            ))}
-          </div>
-        ))}
-        <div className="grid-line--horizontal" />
-        <div className="grid-line--vertical" />
+    <div>
+      <div className="grid">
+        <div className="grid-container">
+          {gridData.map((rowData, rowIndex) => (
+            <div key={rowIndex} className="grid-row">
+              {rowData.map((colData, colIndex) => (
+                <span
+                  key={colIndex}
+                  onClick={() => handleClick(rowIndex, colIndex)}
+                  className={`grid-cell ${rowIndex === 0 ? 'grid-cell--top' : ''} ${colIndex === 49 ? 'grid-cell--right' : ''} ${rowIndex === 49 ? 'grid-cell--bottom' : ''} ${colIndex === 0 ? 'grid-cell--left' : ''} ${selectedRow === rowIndex && selectedCol === colIndex ? 'grid-cell--selected' : ''}`}
+                />
+              ))}
+            </div>
+          ))}
+          <div className="grid-line--horizontal" />
+          <div className="grid-line--vertical" />
+        </div>
       </div>
-      <button onClick={()=> handleSubmitButton(selectedRow, selectedCol)}>Submit</button>
+      {showSubmitButton && (
+          <div className="submit-button-container">
+            <button className="submit-button" onClick={()=> handleSubmitButton(selectedRow, selectedCol)}>Submit</button>
+          </div>
+        )}
     </div>
   );
 }
 
 export default Grid;
+
 
 
 
