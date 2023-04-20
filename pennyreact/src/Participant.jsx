ba4614f98542209677782b490bb0c6de60534bdd
Tyler Vu <tv3433@princeton.edu>
2023-04-20 18:30:26 -0400

    logo

--- pennyreact/src/Participant.jsx
+++ pennyreact/src/Participant.jsx
@@ -6,12 +6,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Participant.css';
-import Navbar from './Navbar';
+import Logo from './ParticipantLogo';
 
 function Participant() {
     return (
         <div className="Participant">
-            <Navbar />
+            <Logo />
             <h1>Welcome to Emotions Net!</h1>
             <p>Thank you for visiting our booth.</p>
             <Link to="/participant/presurvey" className="my-button">Click Here to Begin</Link>
