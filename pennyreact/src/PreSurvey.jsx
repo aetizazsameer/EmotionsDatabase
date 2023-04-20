5dfd4c4875978fc7941e4245b410a75b059f1251
Tyler Vu <tv3433@princeton.edu>
2023-04-20 05:22:59 -0400

    css formatting

--- pennyreact/src/PreSurvey.jsx
+++ pennyreact/src/PreSurvey.jsx
@@ -8,14 +8,18 @@ import { Link } from 'react-router-dom';
 import PennyHeader from './PennyHeader';
 import PennyFooter from './PennyFooter';
 import Grid from './Grid';
+import './PreSurvey.css';
 
 function PreSurvey() {
     return (
-        <div>
-            <PennyHeader />
-            <Grid />
-            <Link to="/participant/video">Video Page</Link>
-            <PennyFooter />
+        <div className="container">
+            <div className="instructions">
+                <h1>Instructions</h1>
+                <p>These are the instructions for the survey. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut in ipsum eu lorem malesuada efficitur at quis dolor. Donec varius quam ac felis consectetur, eget fringilla lorem fringilla. Vestibulum in ultricies ex. Praesent malesuada odio vel nunc hendrerit, ac luctus nulla dapibus. Sed euismod aliquet urna, sit amet imperdiet augue bibendum sed. Vivamus semper massa et libero venenatis, id euismod lectus lobortis. </p>
+            </div>
+            <div className="grid">
+                <Grid />
+            </div>
         </div>
     );
 }
