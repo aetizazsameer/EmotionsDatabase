1d9d178f77a91e19150e3cd84d98beb046bb7339
Tyler Vu <tv3433@princeton.edu>
2023-04-28 02:14:50 -0400

    Search functionality implemented

--- pennyreact/src/ResearcherTable.jsx
+++ pennyreact/src/ResearcherTable.jsx
@@ -9,6 +9,7 @@ import { Link } from "react-router-dom";
 
 const ResearcherTable = () => {
   const [videoData, setVideoData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
   const [sortConfig, setSortConfig] = useState({ key: "id", direction: "ascending" });
 
   useEffect(() => {
@@ -46,71 +47,84 @@ const ResearcherTable = () => {
     return "";
   };
 
+  // Filter videoData based on the search term
+  const filteredVideoData = videoData.filter((video) =>
+    video.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th className={getColumnClassName("id")} onClick={() => sortData("id")}>
-            ID
-          </th>
-          <th className={getColumnClassName("title")} onClick={() => sortData("title")}>
-            Title
-          </th>
-          <th
-            className={getColumnClassName("avg_valence_initial")}
-            onClick={() => sortData("avg_valence_initial")}
-          >
-            Avg Valence Initial
-          </th>
-          <th
-            className={getColumnClassName("avg_valence_final")}
-            onClick={() => sortData("avg_valence_final")}
-          >
-            Avg Valence Final
-          </th>
-          <th
-            className={getColumnClassName("avg_valence_delta")}
-            onClick={() => sortData("avg_valence_delta")}
-          >
-            Avg Valence Delta
-          </th>
-          <th
-            className={getColumnClassName("avg_arousal_initial")}
-            onClick={() => sortData("avg_arousal_initial")}
-          >
-            Avg Arousal Initial
-          </th>
-          <th
-            className={getColumnClassName("avg_arousal_final")}
-            onClick={() => sortData("avg_arousal_final")}
-          >
-            Avg Arousal Final
-          </th>
-          <th
-            className={getColumnClassName("avg_arousal_delta")}
-            onClick={() => sortData("avg_arousal_delta")}
-          >
-            Avg Arousal Delta
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {videoData.map((video) => (
-          <tr key={video.id}>
-            <td>
-              <Link to={`/researcher/${video.id}`}>{video.id}</Link>
-            </td>
-            <td>{video.title}</td>
-            <td>{video.avg_valence_initial.toFixed(2)}</td>
-            <td>{video.avg_valence_final.toFixed(2)}</td>
-            <td>{video.avg_valence_delta.toFixed(2)}</td>
-            <td>{video.avg_arousal_initial.toFixed(2)}</td>
-            <td>{video.avg_arousal_final.toFixed(2)}</td>
-            <td>{video.avg_arousal_delta.toFixed(2)}</td>
+    <div>
+      <input
+        type="text"
+        placeholder="Search by title"
+        value={searchTerm}
+        onChange={(event) => setSearchTerm(event.target.value)}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th className={getColumnClassName("id")} onClick={() => sortData("id")}>
+              ID
+            </th>
+            <th className={getColumnClassName("title")} onClick={() => sortData("title")}>
+              Title
+            </th>
+            <th
+              className={getColumnClassName("avg_valence_initial")}
+              onClick={() => sortData("avg_valence_initial")}
+            >
+              Avg Valence Initial
+            </th>
+            <th
+              className={getColumnClassName("avg_valence_final")}
+              onClick={() => sortData("avg_valence_final")}
+            >
+              Avg Valence Final
+            </th>
+            <th
+              className={getColumnClassName("avg_valence_delta")}
+              onClick={() => sortData("avg_valence_delta")}
+            >
+              Avg Valence Delta
+            </th>
+            <th
+              className={getColumnClassName("avg_arousal_initial")}
+              onClick={() => sortData("avg_arousal_initial")}
+            >
+              Avg Arousal Initial
+            </th>
+            <th
+              className={getColumnClassName("avg_arousal_final")}
+              onClick={() => sortData("avg_arousal_final")}
+            >
+              Avg Arousal Final
+            </th>
+            <th
+              className={getColumnClassName("avg_arousal_delta")}
+              onClick={() => sortData("avg_arousal_delta")}
+            >
+              Avg Arousal Delta
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {filteredVideoData.map((video) => (
+            <tr key={video.id}>
+              <td>
+                <Link to={`/researcher/${video.id}`}>{video.id}</Link>
+              </td>
+              <td>{video.title}</td>
+              <td>{video.avg_valence_initial.toFixed(2)}</td>
+              <td>{video.avg_valence_final.toFixed(2)}</td>
+              <td>{video.avg_valence_delta.toFixed(2)}</td>
+              <td>{video.avg_arousal_initial.toFixed(2)}</td>
+              <td>{video.avg_arousal_final.toFixed(2)}</td>
+              <td>{video.avg_arousal_delta.toFixed(2)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
