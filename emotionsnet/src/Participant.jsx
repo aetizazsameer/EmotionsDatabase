de6e4b1844f2697fe087809d6dfad71cf9bc75b2
Tyler Vu <tv3433@princeton.edu>
2023-05-01 17:23:25 -0400

    fixed Navbar on /participant

--- emotionsnet/src/Participant.jsx
+++ emotionsnet/src/Participant.jsx
@@ -11,12 +11,13 @@ import Logo from './ParticipantLogo';
 
 function Participant() {
     return (
-        <div className="Participant">
+        <div className="page-container">
             <Navbar />
-            <Logo />
+            <div className="Participant">
             <h1>Welcome to Emotions Net!</h1>
             <p>Thank you for visiting our booth.</p>
             <Link to="/participant/presurvey" className="my-button">Click Here to Begin</Link>
+            </div>
         </div>
     );
 }
