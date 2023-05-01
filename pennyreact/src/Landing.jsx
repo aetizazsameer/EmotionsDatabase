a1a565242f5060afbbc6993b1d096b24a99f7fd8
(´・◡・｀) <67237406+aetizazsameer@users.noreply.github.com>
2023-05-01 17:01:27 -0400

    Remove Penny

--- pennyreact/src/Landing.jsx
+++ pennyreact/src/Landing.jsx
@@ -1,23 +1,19 @@
 //----------------------------------------------------------------------
-// landing.js
+// Landing.jsx
 // Author: Aetizaz Sameer, Andrew Hwang, Tyler Vu
 //----------------------------------------------------------------------
 
 import React from 'react';
-import PennyHeader from './PennyHeader';
-import PennyFooter from './PennyFooter';
 
 function Landing() {
     return (
         <div>
-            <PennyHeader />
             <a href="/admin">Admin login</a>
             <br />
             <a href="/researcher">Researcher login</a>
             <br />
             <a href="/participant">Participant login</a>
             <br />
-            <PennyFooter />
         </div >
     );
 }
